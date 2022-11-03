35be9c8c7cd0e0da7720587f95f9ad54dd435079
Richard Chuo <richard.chuo@gmail.com>
2022-11-03 08:01:39 +0800

    Additional changes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
 // happy coding 👻
-console.log("hello world");
+console.log('hello world')
+
+// data Tiger = EmployeeId Name
+// [Tiger] -> [Tigers]
+
+import { Newtype, prism } from 'newtype-ts'
+import * as O from 'fp-ts/Option'
+import { pipe } from 'fp-ts/function'
+import { shuffle } from 'radash'
+
+const isNonEmptyString: (str: string) => boolean = (str) => str !== '' && str !== null && str !== undefined
+
+interface EmployeeId extends Newtype<{ readonly EmployeeId: unique symbol }, string> {}
+
+const employeeIdOf = prism<EmployeeId>(isNonEmptyString).getOption
+//    ^?
+
+interface Name extends Newtype<{ readonly Name: unique symbol }, string> {}
+
+const nameOf = prism<Name>(isNonEmptyString).getOption
+//    ^?
+
+type Tiger = Readonly<{ employeeId: EmployeeId; name: Name }>
+//   ^?
+
+const buildTiger: (eId: EmployeeId) => (name: Name) => Readonly<Tiger> = (employeeId) => (name) => ({ employeeId, name })
+const tigerOf: (employeeIdOpt: O.Option<EmployeeId>) => (nameOpt: O.Option<Name>) => O.Option<Tiger> = (eo) => (no) =>
+  pipe(O.of(buildTiger), O.ap(eo), O.ap(no))
+
+console.log(JSON.stringify(employeeIdOf('')))
+console.log(JSON.stringify(nameOf('Richard Chuo')))
+console.log(JSON.stringify(tigerOf(employeeIdOf('118520'))(nameOf('Richard Chuo'))))
+console.log(JSON.stringify(tigerOf(employeeIdOf('118520'))(nameOf(''))))
+
+const lucyDraw: (tigers: Readonly<Array<Tiger>>) => Array<Tiger> = (tigers) => shuffle(tigers)
+
+const Richard = tigerOf(employeeIdOf('118529'))(nameOf('Richard'))
+const Jane = tigerOf(employeeIdOf('117522'))(nameOf('Jane'))
+const John = tigerOf(employeeIdOf('125323'))(nameOf('John'))
+
+const tigers = O.sequenceArray([Richard, Jane, John])
+//    ^?
+
+console.log(JSON.stringify(pipe(tigers, O.map(lucyDraw))))
+//                                        ^?
